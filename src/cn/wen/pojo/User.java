71dfe6f04ba644d94208915e7cbe95392dfb2ff8
zzw0729 <1779684450@qq.com>
2019-09-04 12:06:23 +0800

    idea第二次提交

---
+++ src/cn/wen/pojo/User.java
@@ -0,0 +1,12 @@
+package cn.wen.pojo;
+
+/**
+ * @author: zhangzhiwen
+ * @date: 2019-09-04 11:57
+ */
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("woshi");
+    }
+}
